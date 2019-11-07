1790430287725fe1b712ff6e8dc5bc1b0a2db15b
linux-insideDE <git@linux-inside.de>
2019-11-07 21:40:31 +0100

    refactored parser

--- fileparser.go
+++ fileparser.go
@@ -8,61 +8,77 @@ import (
 	"strings"
 )
 
-func parseFile(scnr *bufio.Scanner, argset argumentSet, file *os.File) (match listEntry) {
+func parseFile(scanner *bufio.Scanner, argset argumentSet, file *os.File) (match listEntry) {
 	match.match = false
 	match.release = false
 
-	for scnr.Scan() {
+	for scanner.Scan() {
 		switch {
-		case strings.HasPrefix(scnr.Text(), "#"):
-			if strings.HasPrefix(scnr.Text(), "# Maintainer  ") {
-				match.maintainer = strings.TrimSpace(strings.ReplaceAll(strings.TrimPrefix(scnr.Text(), "# Maintainer"), ":", ""))
-			}
+		case strings.HasPrefix(scanner.Text(), "#"):
+			parseMetadataLine(file, scanner, &match)
 
-			if strings.HasPrefix(scnr.Text(), "# Maintainer URL") {
-				match.url = strings.TrimSpace(strings.Replace(strings.TrimPrefix(scnr.Text(), "# Maintainer URL"), ":", "", 1))
-			}
-
-			if strings.HasPrefix(scnr.Text(), "# List source URL") {
-				if tmpurl := strings.TrimSpace(strings.Replace(strings.TrimPrefix(scnr.Text(), "# List source URL"), ":", "", 1)); tmpurl != "" {
-					match.url = tmpurl
-				}
-			}
-
-			if strings.HasPrefix(scnr.Text(), "# Category") {
-				match.category = strings.TrimSpace(strings.ReplaceAll(strings.TrimPrefix(scnr.Text(), "# Category"), ":", ""))
-			}
-
-			// Parse geolocation to country
-			if match.category == "geolocation" {
-				tmp0 := strings.TrimSuffix(filepath.Base(file.Name()), ".netset")
-				tmp1 := strings.Split(tmp0, "_")
-				match.country = strings.ToUpper(tmp1[len(tmp1)-1])
-			}
-
-			// Parse Listname
-			match.list = strings.TrimSuffix(strings.TrimSuffix(filepath.Base(file.Name()), ".netset"), ".ipset")
-		case scnr.Bytes()[0] == 0x04:
+		case scanner.Bytes()[0] == 0x04:
 			break
 		default:
-			_, parsedNet, err := net.ParseCIDR(scnr.Text())
-			if err == nil && parsedNet.Contains(argset.inputIP) {
-				match.match = true
-				match.ip = parsedNet.String()
-
-				return match
-			}
-
-			if parsedIP := net.ParseIP(scnr.Text()); parsedIP != nil {
-				if parsedIP.Equal(argset.inputIP) {
-					match.match = true
-					match.ip = parsedIP.String()
-
-					return match
-				}
-			}
+			parseIPAndNet(scanner.Text(), argset, &match)
 		}
 	}
 
 	return match
+}
+
+func getURL(text, token string) string {
+	return strings.TrimSpace(strings.Replace(strings.TrimPrefix(text, token), ":", "", 1))
+}
+
+func getContent(text, token string) string {
+	return strings.TrimSpace(strings.ReplaceAll(strings.TrimPrefix(text, token), ":", ""))
+}
+
+func parseMetadataLine(file *os.File, scanner *bufio.Scanner, match *listEntry) {
+	if strings.HasPrefix(scanner.Text(), "# Maintainer  ") {
+		match.maintainer = getContent(scanner.Text(), "# Maintainer")
+	}
+
+	if strings.HasPrefix(scanner.Text(), "# Maintainer URL") {
+		match.url = getURL(scanner.Text(), "# Maintainer URL")
+	}
+
+	if strings.HasPrefix(scanner.Text(), "# List source URL") {
+		if tmpurl := getURL(scanner.Text(), "# List source URL"); tmpurl != "" {
+			match.url = tmpurl
+		}
+	}
+
+	if strings.HasPrefix(scanner.Text(), "# Category") {
+		match.category = getContent(scanner.Text(), "# Category")
+	}
+
+	if match.category == "geolocation" {
+		pureFilename := strings.TrimSuffix(filepath.Base(file.Name()), ".netset")
+		splittedFilename := strings.Split(pureFilename, "_")
+		match.country = strings.ToUpper(splittedFilename[len(splittedFilename)-1])
+	}
+
+	// Parse Listname
+	match.list = strings.TrimSuffix(strings.TrimSuffix(filepath.Base(file.Name()), ".netset"), ".ipset")
+}
+
+func parseIPAndNet(text string, argset argumentSet, match *listEntry) {
+	_, parsedNet, err := net.ParseCIDR(text)
+	if err == nil && parsedNet.Contains(argset.inputIP) {
+		match.match = true
+		match.ip = parsedNet.String()
+
+		return
+	}
+
+	if parsedIP := net.ParseIP(text); parsedIP != nil {
+		if parsedIP.Equal(argset.inputIP) {
+			match.match = true
+			match.ip = parsedIP.String()
+
+			return
+		}
+	}
 }
